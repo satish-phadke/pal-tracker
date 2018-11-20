b262b32fd517ac62d44819ebe60d0c35de61b9ae
sashinpivotal <sashin@pivotal.io>
2018-11-19 19:45:47 -0500

    Hello

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,4 +1,17 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WelcomeController {
+
+    @Value("${WELCOME_MESSAGE}")
+    private String welcomeMessage;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return welcomeMessage + " Good morning" ;
+    }
 }
